b37fd639c47ed3e9e1d678ae73e1c4cbc4074e0b
Данило <danielpanasenko777@gmail.com>
2022-12-12 19:04:43 +0200

    main

--- src/index.js
+++ src/index.js
@@ -43,14 +43,16 @@ function createGallery(images) {
 }
 refs.galleryContainer.insertAdjacentHTML('beforeend', createGallery);
 
-var lightbox = new SimpleLightbox('.gallery a', {
-  /* options */
+const lightBoxOptions = {
   captionsData: 'alt',
-  captionDelay: '250',
+  captionDelay: 250,
   captionPosition: 'bottom',
-});
-const BASE_URL = 'https://pixabay.com/api/';
+  showCounter: true,
+};
+let gallery = new Simplelightbox('.gallery a', lightBoxOptions);
+
 const searchOptions = {
+  BASE_URL: 'https://pixabay.com/api/',
   key: '31991088-557b1f719244b12b6790ca772',
   q: '',
   image_type: 'photo',
@@ -58,4 +60,28 @@ const searchOptions = {
   safesearch: true,
   per_page: 40,
   page: 1,
+
+  async getImagesByQuery(page) {
+    const { data } = await axios.get(
+      `${searchOptions.BASE_URL}?key=${searchOptions.key}&q=${searchOptions.q}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${searchOptions.page}`
+    );
+
+    return data;
+  },
 };
+
+refs.form.addEventListener('sumbit', onFormSubmit);
+async function onFormSubmit(e) {
+  e.preventDefault();
+  const {
+    elements: { searchQuery },
+  } = e.currentTarget;
+  const searchValue = searchQuery.value.trim().toLowerCase();
+  try {
+    const collection = await getImagesByQuery();
+    console.log(collection);
+  } catch (error) {
+    console.log(error);
+  }
+  refs.form.reset();
+}
